36b75588c2e8c131d1b70c5b0e408fa01efc1b63
jcu011 <jcu011@naver.com>
2020-07-26 21:58:59 +0900

    Update Solved P & Library

--- PS_library/LIS.cpp
+++ PS_library/LIS.cpp
@@ -1,8 +1,6 @@
 // <반도체 설계> - BOJ_2352
-// LIS(Longest Increase Subsequence) : 이분탐색 이용
+// LIS(Longest Increase Subsequence) : 이분탐색 이용 O(NlogN)
 // 최대값에 -INF 넣는거 확인
-
-// 미완
 
 #include<bits/stdc++.h>
 using namespace std;
@@ -22,6 +20,7 @@ int main() {
     for(int i=0; i<N; i++)
         cin>>m[i];
 
+    // O(N^2) 알고리즘 : dp[i]-> i번째 수까지 LIS크기
     /*
     for (int i = 0; i < N; i++) {
         if (dp[i] == 0) dp[i] = 1;
@@ -40,18 +39,20 @@ int main() {
     cout<<max_;
     */
 
+
+    // O(NlogN)
     int ans =0;
     vt.push_back(-INF);
     for (int i = 0; i < N; i++) {
-        if (vt.back() < m[i]) {
+        // 탐색할 수(m[i])가 배열(vt)의 마지막 수보다 큰 경우, vt뒤에 붙임
+        if (vt.back() < m[i]) { 
             vt.push_back(m[i]);
             ans++;
         }
-        else {
+        else { // 마지막 수보다 작은경우, 수가 더 작은 증가수열을 만듦. ex) m[i]가 3이고 vt:(1,2,7) ->  vt:(1,2,3)으로 바뀜
             auto it = lower_bound(vt.begin(), vt.end(), m[i]);
             *it = m[i];
         }
     }
-
     cout<<'\n'<<ans;
 }
