a66164957538277366fbaa13a75ad3eea3a996f3
jcu011 <jcu011@naver.com>
2020-08-16 18:19:15 +0900

    update solved P

--- BackJun_PS/ParametricSearch/BOJ_1300.cpp
+++ BackJun_PS/ParametricSearch/BOJ_1300.cpp
@@ -1,36 +1,25 @@
-// <K번째 수> - BOJ_1300
-// Parametric search
+// <K번째 수>
+// 이분탐색
 
 #include<bits/stdc++.h>
 using namespace std;
-typedef long long ll;
 
 int N,K;
-
-bool isPossible(int mid){
-    int cnt=0;
-    for(int i=1; i<=N; i++){
-        if(mid/i>N) cnt+=N;
-        else cnt+=(mid/i);
-    }
-    return cnt<K;
-}
-
-int b_search(int l, int r){
-    int s=l,e=r;
-    int mid;
-
-    while(s<=e){
-        mid = (s+e)/2;
-        if(isPossible(mid)) s = mid+1;
-        else                e = mid-1;
-    }
-    return s;
-}
 
 int main() {
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cin>>N>>K;
-    cout<<b_search(1,K);
+    int l=1, r=K, mid;
+    while(l<=r){
+        mid = (l+r)/2;
+        int cnt = 0;
+        for(int i=1; i<=N; i++){
+            if(mid/i>N){cnt+=N;}
+            else{cnt+=mid/i;}
+        }
+        if(cnt<K) l=mid+1;
+        else r=mid-1;
+    }
+    cout<<l;
 }
