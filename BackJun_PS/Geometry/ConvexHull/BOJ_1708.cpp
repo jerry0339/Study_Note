39933c550ad5d0270f515d9379d3bc5f8acb4a74
jcu011 <jcu011@naver.com>
2020-11-13 01:18:31 +0900

    update solved P & Library

--- BackJun_PS/Geometry/ConvexHull/BOJ_1708.cpp
+++ BackJun_PS/Geometry/ConvexHull/BOJ_1708.cpp
@@ -1,5 +1,5 @@
 // <볼록 껍질> - BOJ_1708
-// ConvexHull : Graham Scan
+// ConvexHull : graham scan
 
 #include <bits/stdc++.h>
 #define all(v) v.begin(),v.end()
@@ -12,6 +12,7 @@ typedef long long ll;
 typedef pair<int,int> pii;
 
 int N;
+pii mpt;
 vector<pii> v;
 
 int ccw(pii p, pii a, pii b){
@@ -19,13 +20,20 @@ int ccw(pii p, pii a, pii b){
     return t ? (t>0 ? 1 : -1) : 0;
 }
 
-pii mpt;
 bool comp(const pii &a, const pii &b){
     int ret = ccw(mpt, a, b);
     return ret > 0  || (ret==0 && a < b);
 }
 
-void convexHull(){
+int main() {
+	ios_base::sync_with_stdio(false);
+    cin.tie(0);
+	cin>>N;
+	int a,b;
+	for(int i=0; i<N; i++){
+		cin>>a>>b;  
+		v.eb(pii(a,b));
+	}
     mpt = *min_element(all(v));
     sort(all(v),comp);
 
@@ -39,17 +47,4 @@ void convexHull(){
         ans.eb(v[i]);
     }
     cout<<sz(ans);
-}
-
-int main() {
-	ios_base::sync_with_stdio(false);
-    cin.tie(0);
-	cin>>N;
-	int a,b;
-	for(int i=0; i<N; i++){
-		cin>>a>>b;  
-		v.eb(pii(a,b));
-	}
-    
-    convexHull();
 }
