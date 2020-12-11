5d9ae87dafe8b90d0e0a54f8df3e76fd1ef1d9db
jcu011 <jcu011@naver.com>
2020-12-12 03:54:35 +0900

    update library

--- PS_library/Tree/SegmentTree/segmentTree.cpp
+++ PS_library/Tree/SegmentTree/segmentTree.cpp
@@ -1,65 +1,69 @@
 // <커피숍 2> - BOJ_1275
-// 세그먼트 트리 (재귀)
+// 세그먼트 트리 (재귀 - 직관적임)
 
-// [Classic Segment Tree](http://codeforces.com/blog/entry/15890)
-// [Efficient Segment Tree](http://codeforces.com/blog/entry/18051)
-// [Data Structures](http://codeforces.com/blog/entry/15729)
-
-#include <bits/stdc++.h>
-#define rep(i,x,n) for(int i=x; i<n; i++)
+#include<bits/stdc++.h>
+#define MAX_N 100000 // MAX값 확인
 using namespace std;
+typedef long long ll;
 
+ll arr[MAX_N]{}, tree[MAX_N<<2]{};
+ll N,Q;
 
-long long init(vector<long long> &a, vector<long long> &tree, int node, int start, int end) {
-    if (start == end) {
-        return tree[node] = a[start];
-    } else {
-        return tree[node] = init(a, tree, node*2, start, (start+end)/2) + init(a, tree, node*2+1, (start+end)/2+1, end);
-    } 
+ll init(ll n, ll s, ll e){
+    if(s==e) return tree[n] = arr[s];
+    ll mid = (s+e)>>1;
+    // 재귀적으로 두 부분으로 나눈 뒤에 그 합을 자기 자신으로 함.
+    return tree[n] = init(n<<1, s, mid) + init(n<<1|1, mid+1, e);
 }
 
-long long sum(vector<long long> &tree, int node, int start, int end, int left, int right) {
-    if (left > end || right < start) { 
-        return 0;
-    }
-    if (left <= start && end <= right) {
-        return tree[node];
-    }
-    return sum(tree, node*2, start, (start+end)/2, left, right) + sum(tree, node*2+1, (start+end)/2+1, end, left, right);
+// s: 시작 인덱스, e: 끝 인덱스
+// idx: 구간 합을 수정하고자 하는 노드
+// diff: 수정할 값
+void modify(ll n, ll s, ll e, ll idx, ll diff){
+    // 범위 밖에 있는 경우
+    if(idx<s || idx>e) return;
+    // 범위 안에 있으면 내려가며 다른 원소도 갱신
+    tree[n] += diff;
+    if(s==e) return;
+    ll mid = (s+e)>>1;
+    modify(n<<1, s, mid, idx, diff);
+    modify(n<<1|1, mid+1, e, idx, diff);
 }
 
-void update(vector<long long> &tree, int node, int start, int end, int index, long long diff) {
-    if (index < start || index > end) return;
-    tree[node] = tree[node] + diff;
-    if (start != end) {
-        update(tree,node*2, start, (start+end)/2, index, diff);
-        update(tree,node*2+1, (start+end)/2+1, end, index, diff);
-    }
+// s: 시작 인덱스, e: 끝 인덱스
+// l, r: 구간 합을 구하고자 하는 범위
+ll query(ll n, ll s, ll e, ll l, ll r){
+    // 범위 밖에 있는 경우
+    if(l>e || r<s) return 0;
+    // 범위 안에 있는 경우
+    if(l<=s && e<=r) return tree[n];
+    // 그렇지 않다면 두 부분으로 나누어 합을 구하기
+    ll mid = (s+e)>>1;
+    return query(n<<1, s, mid, l, r) + query(n<<1|1, mid+1, e, l, r);
 }
 
 int main() {
-	ios_base::sync_with_stdio(false); cin.tie(nullptr);
-	int N, Q;
-	cin>>N>>Q;
-    vector<long long> arr(N);
-	int h = (int)ceil(log2(N));
-    int tree_size = (1 << (h+1));
-    vector<long long> tree(tree_size);
-    for(int i=0; i<N; i++) cin>>arr[i];
-    init(arr, tree, 1, 0, N-1);
-    while(Q--) {
-        int x,y,a,b;
-        cin>>x>>y>>a>>b;
-        if(x>y) {
-            long long t = x;
-            x = y;
-            y = t;
-        }
-        cout<<sum(tree, 1, 0, N-1, x-1, y-1)<<'\n';
-        a--;
-        long long diff = b-arr[a];
-        arr[a] = b;
-        update(tree, 1, 0, N-1, a, diff);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cin>>N>>Q;
+    // 인덱싱은 0부터 안해도 됨.
+    // [1, N] 과 같이 인덱싱이 가능
+    for(ll i=0; i<N; i++){
+        cin>>arr[i];
     }
-    
+    init(1,0,N-1);
+
+    ll x,y,a;
+    ll b;
+    while(Q--){
+        cin>>x>>y>>a;
+        cin>>b;
+        if(x>y) swap(x,y);
+        ll diff = b - arr[a-1];
+        arr[a-1] = b;
+        cout<<query(1, 0, N-1, x-1, y-1)<<'\n';
+        modify(1, 0, N-1, a-1, diff);
+    }
+
+    return 0;
 }
