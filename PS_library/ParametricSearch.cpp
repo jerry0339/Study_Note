2a6cef8b5249051c0bc78fa155bf0bd72de0186f
jcu011 <jcu011@naver.com>
2021-02-25 02:50:07 +0900

    update

--- PS_library/ParametricSearch.cpp
+++ PS_library/ParametricSearch.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 int N;
 
-bool ispossible(int mid){
+bool isP(int mid){
     int cnt = 0;
     // calculate condition (cnt)
     
@@ -23,21 +23,24 @@ int b_search(int l, int r)
     int s = l, e = r, mid;
     while (s <= e) {
         mid = (s + e) / 2;
-        if (ispossible(mid)) s = mid + 1;
+        if (isP(mid)) s = mid + 1;
         else                 e = mid - 1;
 
-        /*
-        if(v[mid] < val)      s = mid + 1;
-        else if(v[mid > val]) e = mid - 1;
-        else { // v[mid] == val
-            // == 따로 정의
-            // ex) v = {1, 2, 2, 2, 3}; 에서 2를 search할때, mid(idx)가 1,2,3일때 s와 e의 idx이동을 정의
-        }
-        */
     }
     return s; // lower_bound, upper_bound(s = mid + 1;에 =포함)
     // ex) v = {1, 2, 2, 2, 3}; 에서 2를 search할때, lower_bound는 idx 1, upper_bound는 idx 4가 출력됨
 
     // return e; // upper_bound(=포함) - 1
     // ex) v = {1, 2, 2, 2, 3}; 에서 2를 search할때, upper_bound-1 출력시 idx 3이 출력됨
+}
+
+int lowerBoundEx(int l, int r){
+    int s = l, e = r, mid;
+    while(s<=e){
+        mid = (s+e)/2;
+        if(!isP(mid)) s = mid + 1;
+        else e = mid - 1;
+    }
+
+    return s;
 }
