eb07df954d50ad0cc6e069b1dd7c3334169518ca
jcu011 <jcu011@naver.com>
2021-03-30 16:01:05 +0900

    update lazy seg

--- PS_library/Tree/SegmentTree/Lazy-Propagation.cpp
+++ PS_library/Tree/SegmentTree/Lazy-Propagation.cpp
@@ -9,16 +9,18 @@ typedef long long ll;
 
 ll arr[MAX_N]{};
 ll tree[MAX<<2]{};
-ll lazy[MAX_N<<2]{};
+ll lazy[MAX<<2]{};
 ll N,M,K,T;
 
+// 바뀐것 없음
 ll init(ll n, ll s, ll e){
     if(s==e) return tree[n] = arr[s];
     ll mid = (s+e)>>1;
     return tree[n] = init(n<<1, s, mid) + init(n<<1|1, mid+1, e);
 }
 
-void propagate(ll n, ll s, ll e) {
+// lazy[n]을 tree[n]에 업데이트, lazy[n<<1], lazy[n<<1|1]에 전파
+void propagate(ll n, ll s, ll e) { 
     if(lazy[n]){
         tree[n] += (e-s+1)*lazy[n];
         if(s!=e){
@@ -29,25 +31,28 @@ void propagate(ll n, ll s, ll e) {
     }
 }
 
-void lazyUpdate(ll n, ll s, ll e, ll l, ll r, ll add) {
-    propagate(n,s,e);
-    if(r<s || l>e) return;
-    if(l<=s && e<=r){
-        tree[n] += (e-s+1)*add;
-        if(s!=e){
-            lazy[n<<1] += add;
-            lazy[n<<1|1] += add;
+// [l,r]구간에 diff를 더하는 업데이트 요청
+// 호출이 끝나면 현재 노드(n)는 valid여야 한다.
+void lazyUpdate(ll n, ll s, ll e, ll l, ll r, ll diff) {
+    propagate(n, s, e); // 전파 : 현재 노드를 유효하게 만듬
+    if(r<s || e<l) return; // 범위 밖
+    if(l<=s && e<=r){ // 현재구간[s,e]이 포함되어 있으면
+        tree[n] += (e-s+1)*diff; // 현재 구간 갱신
+        if(s!=e) { // 현재노드가 리프노드가 아닐경우 자식 update
+            lazy[n<<1] += diff;
+            lazy[n<<1|1] += diff;
         }
         return;
     }
     ll mid = (s+e)>>1;
-    lazyUpdate(n<<1, s, mid, l, r, add);
-    lazyUpdate(n<<1|1, mid+1, e, l, r,add);
-    tree[n] = tree[n<<1]+tree[n<<1|1];
+    lazyUpdate(n<<1, s, mid, l, r, diff);
+    lazyUpdate(n<<1|1, mid+1, e, l, r, diff);
+    tree[n] = tree[n<<1] + tree[n<<1|1];
 }
 
+
 ll query(ll n, ll s, ll e, ll l, ll r){
-    propagate(n, s, e);
+    propagate(n, s, e); // 현재노드의 lazy값 전파 -> valid
     if(l>e || r<s) return 0;
     if(l<=s && e<=r) return tree[n];
     ll mid = (s+e)>>1;
@@ -62,7 +67,7 @@ int main() {
         cin>>arr[i];
     }
     init(1,1,N);
-
+    
     ll a,x,y,diff;
     T = M+K;
     while(T--){
