bcc1ea2184a26875e8ae33f341cf0924dc26474c
jcu011 <jcu011@naver.com>
2021-06-12 19:17:17 +0900

    [ADD] 210612

---
+++ BackJun_PS/Graph/Bellman-Ford/BOJ_1865.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+#define xx first
+#define yy second
+using namespace std;
+using pii = pair<int,int>;
+using tpi = tuple<int,int,int>;
+const int INF = 0x3f3f3f3f;
+
+int TC,N,M,W;
+vector<tpi> edge;
+int dist[501]{};
+
+bool BellmanFord(int st){
+    memset(dist,0x3f,sizeof(dist));
+    dist[st] = 0;
+    queue<int> q;
+    q.push(st);
+
+}
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    int S,E,T;
+    cin>>TC;
+    while(TC--){
+        edge.clear();
+        cin>>N>>M>>W;
+        for(int i=0; i<M; i++){
+            cin>>S>>E>>T;
+            edge.push_back({S,E,T}); // 양방향 간선
+            edge.push_back({E,S,T});
+        }
+        for(int i=0; i<W; i++){
+            cin>>S>>E>>T;
+            edge.push_back({S,E,-T}); // 웜홀은 방향 있음
+        }
+        
+        bool res = false;
+        for(int i=1; i<=N; i++){ // 모든 노드를 시작점으로 확인
+            if(!BellmanFord(i)) {
+                res = true;
+                break;
+            }
+        }
+
+        if(!res) cout<<"NO"<<'\n';
+        else cout<<"YES"<<'\n';
+    }
+
+}
