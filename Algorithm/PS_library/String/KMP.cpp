7015338fad40e6fca812ef7d176d3f380e8b3fdf
전찬의 <jcu011@naver.com>
2022-05-10 22:52:55 +0900

    Mod KMP

--- Algorithm/PS_library/String/KMP.cpp
+++ Algorithm/PS_library/String/KMP.cpp
@@ -1,50 +1,45 @@
 // https://www.acmicpc.net/problem/1786
-// KMP : 커누스 모리스 프랫 알고리즘(Knuth-Morris-Pratt algorithm)
-// 문자열 m에서 n문자열 검색을 목적으로, 문자열m의 길이:M, 문자열n의 길이:N
-// getPi(string m) : 접두, 접미 정보 반환, O(M)
-// kmp(string m, string n) : 문자열m에서 문자열n이 존재한지 검색하여 위치한 인덱스들을 반환, O(M+N)
+// KMP 알고리즘
+// O(M+N) : M -> 문자열s길이, N -> 문자열p길이
 
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int> getPi(string p){ // O(M) , M = p.size()
-    int m = (int)p.size(), j=0;
-    vector<int> pi(m, 0);
-    for(int i = 1; i< m ; i++){ // i: 부분 문자열(0~i 범위의 문자열)의 마지막 문자 인덱스
-        while(j > 0 && p[i] != p[j]) j = pi[j-1]; // 이해하기 어려움 구글링ㄱㄱ
-        if(p[i] == p[j]) pi[i] = ++j;
+vector<int> getPI(string p) {
+    int m=p.size(), j=0;
+    vector<int> fail(m,0);
+    //p와 p끼리 KMP를 수행
+    for(int i=1; i<m; i++) {
+        while(j>0 && p[i]!=p[j]) j = fail[j-1];
+        if(p[i] == p[j]) fail[i] = ++j;
     }
-    return pi;
+    return fail;
 }
 
-vector<int> kmp(string s, string p){ // O(M+N) , M = p.size(), N = s.size()
-    vector<int> ans;
-    auto pi = getPi(p); // O(M)
-    int n = (int)s.size(), m = (int)p.size(), j =0;
-
-    // getPi와 똑같은 원리 : O(N)
-    for(int i = 0 ; i < n ; i++){ 
-        while(j>0 && s[i] != p[j]) j = pi[j-1];
-        if(s[i] == p[j]){
-            if(j==m-1){
-                ans.push_back(i-m+1);
-                j = pi[j];
-            } 
-            else j++;
+//문자열s 중에 문자열p가 등장하는 시작 인덱스들이 담긴 벡터를 리턴
+vector<int> KMP(string s, string p) { 
+    vector<int> ret, fail=getPI(p);
+    int n=s.size(), m=p.size(), j=0;
+    for(int i=0 ; i<n; i++) {
+        while(j>0 && s[i]!=p[j]) j = fail[j-1]; // 매칭 중 불일치 발생, 이전의 매칭됐던 문자열 적용
+        if(s[i] == p[j]) { // 한 글자 매칭 성공한 경우
+            if(j == m-1) { // 완전 매칭 성공한 경우
+                ret.push_back(i-m+1);
+                j = fail[j];
+            } else j++;
         } 
     }
-    return ans;
+    return ret;
 }
 
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
-    string str1;
-    string str2;
+    string str1, str2;
     getline(cin,str1);
     getline(cin,str2);
     
-    auto v = kmp(str1, str2);
+    auto v = KMP(str1, str2);
     cout<<v.size()<<'\n';
     for(int i=0; i<v.size(); i++) {
         cout<<v[i]+1<<' ';
